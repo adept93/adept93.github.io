c98f099830e8e673f7407f2c0ba23433db3a7cdd
adept93 <72925262+adept93@users.noreply.github.com>
2020-10-15 16:02:06 +0200

    Add files via upload

---
+++ data/apa75fr_0.js
@@ -0,0 +1,9 @@
+var json_apa75fr_0 = {
+"type": "FeatureCollection",
+"name": "apa75fr_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "NOM COMMUN": "PARIS 12e" }, "geometry": { "type": "Point", "coordinates": [ 2.45303898838703, 48.820059308554214 ] } },
+{ "type": "Feature", "properties": { "id": "2", "NOM COMMUN": "PARIS 16e" }, "geometry": { "type": "Point", "coordinates": [ 2.229307761106053, 48.857002122024106 ] } }
+]
+}
